b9d62f2db03a4ac1f1067da8a0dbc83efc6efaee
Dennis Lutter <dl@figo.io>
2018-02-19 15:51:20 +0100

    stuff ...

--- install.sh
+++ install.sh
@@ -1,2 +1,4 @@
 echo "source $HOME/dotfiles/index" > "$HOME/.zshrc"
 ln -sf "$HOME/dotfiles/.vimrc" "$HOME/.vimrc"
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
