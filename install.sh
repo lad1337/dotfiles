8a9f54ba917930cf795a03188749c5fa0df2f148
Dennis Lutter <dennis.lutter@figo.io>
2019-01-04 16:53:19 +0100

    add fasd and imprve install

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
+set -ex pipefail
+
 echo "source $HOME/dotfiles/index" > "$HOME/.zshrc"
 ln -sf "$HOME/dotfiles/.vimrc" "$HOME/.vimrc"
+echo "Installing VIM plugin manager"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+echo "Installing VIM plugins"
+vim +PlugInstall +qall
