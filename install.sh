8790313a867a8e74837db8c00fd7592b62892aab
Dennis Lutter <lad1337@gmail.com>
2020-01-17 00:51:25 +0100

    use nvim for git and link gitconfig

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 set -ex pipefail
 
+ln -sf "$HOME/dotfiles/.gitconfig" "$HOME/.gitconfig"
 echo "source $HOME/dotfiles/index" > "$HOME/.zshrc"
 ln -sf "$HOME/dotfiles/vim" "$HOME/.config/nvim"
 echo "Installing VIM plugins"
