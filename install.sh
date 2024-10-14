e33b5eb9d127333fa8004d1768471d21e6e6e8d5
lad1337 <lad1337@gmail.com>
2024-10-14 14:14:07 +0200

    add p10k config and nix completion submodule

--- install.sh
+++ install.sh
@@ -4,10 +4,11 @@ ln -sf "$HOME/dotfiles/.gitconfig" "$HOME/.gitconfig"
 echo "source $HOME/dotfiles/index" > "$HOME/.zshrc"
 rm -rf "$HOME/.config/nvim"
 ln -sf "$HOME/dotfiles/vim" "$HOME/.config/nvim"
+ln -sf "$HOME/dotfiles/configs/direnv" "$HOME/.config/direnv"
+ln -sf "$HOME/dotfiles/.p10k.zsh" "$HOME/"
+
 echo "Installing VIM plugins"
 vim +PlugInstall +qall
-
-ln -sf "$HOME/dotfiles/configs/direnv" "$HOME/.config/direnv"
 
 #git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 # brew install git-delta fd rg bat nvim node golang coreutils zsh-syntax-highlighting
